2d238956286e7cd5c5788bdb40abc26faea2f948
lucaslidnei <lucas.nafita@gmail.com>
2026-07-15 21:26:27 -0300

    3. Realizada analise das perguntas de negócio

--- scripts/2_transformar.py
+++ scripts/2_transformar.py
@@ -130,7 +130,7 @@ SQL_CALC_VIAGEM = """
 UPDATE silver_viagem
 
 SET valor_total = COALESCE(valor_diarias, 0) + COALESCE(valor_passagens, 0) 
-                  + COALESCE(valor_devolucao, 0) + COALESCE(valor_outros_gastos, 0),
+                  - COALESCE(valor_devolucao, 0) + COALESCE(valor_outros_gastos, 0),
     duracao_dias  = DATEDIFF(data_fim, data_inicio)
 """
 
